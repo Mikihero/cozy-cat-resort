c9c18e020cacff9b533fe40bf89a0ddc7b2d7c9d
wb-p <wzbury@wp.pl>
2025-12-30 15:57:39 +0100

    The ads work !!

--- Scenes/add_coins.gd
+++ Scenes/add_coins.gd
@@ -3,14 +3,9 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pressed.connect(_button_pressed)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _button_pressed() -> void:
-	Globals.CatCoins += 100
-	Globals.GoldCatCoins += 10
-	Globals.DiamondCatCoins += 1
