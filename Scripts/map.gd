3f8f7df20d001262021ca35ca8ae51de579f0a3a
Piotr Bartoszewicz <piotrek15.pb@gmail.com>
2025-12-30 10:56:17 +0100

    mapka

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,37 +1,17 @@
 extends Node2D
 
-var entities = [
-	#MapEntity.new(Vector2i(0, 0)),
-	MapEntity.new(Vector2i(3, 4)),
-	MapEntity.new(Vector2i(2, 0)),
-	MapEntity.new(Vector2i(3, 7)),
-	MapEntity.new(Vector2i(11, 4)),
-	MapEntity.new(Vector2i(10, 0)),
-	MapEntity.new(Vector2i(11, 6)),
-	MapEntity.new(Vector2i(8, 4)),
-	MapEntity.new(Vector2i(5, 0)),
-	MapEntity.new(Vector2i(9, 3)),
-	MapEntity.new(Vector2i(6, 4)),
-	MapEntity.new(Vector2i(5, 0)),
-	MapEntity.new(Vector2i(7, 7)),
-];
 
-var width = 12
-var height = 8
 
-var path
+var entities = [];
+
+var width = 0
+var height = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var foreground: TileMapLayer = self.get_node("TileMapForeground");
-	for entity in entities:
-		print(entity.position);
-		
-		foreground.set_cell(entity.position, 0, Vector2i(52, 5));
-		
-	path = self.get_best_path(Vector2i(0, 0), Vector2i(width - 1, height - 1))
-	print(path)
-	
+	get_free_tiles()
+			
 	pass # Replace with function body.	
 
 func get_best_path(source: Vector2i, destination: Vector2i) -> Array[Vector2i]:
@@ -85,19 +65,21 @@ func get_best_path(source: Vector2i, destination: Vector2i) -> Array[Vector2i]:
 	var path: Array[Vector2i] = [];
 	return path;
 
+func get_free_tiles() -> Array[Vector2i]: 
+	var background: TileMapLayer = self.get_node("TileMapBackground");
+	var border_tiles = (self.get_node("TileMapBackgroundBorder") as TileMapLayer).get_used_cells();	
+	var positions = background.get_used_cells().filter(func(c): return !border_tiles.has(c));
+	
+	print(border_tiles);	
+	print(background.get_used_cells().size(), "     ", border_tiles.size())
+	print(positions.size())
+	
+	return Array();
+
 func add_entity(entity: MapEntity):
 	pass
 
-var num_calls = 0
-var index = 0
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	num_calls += 1;
-	if num_calls == 60:
-		num_calls = 0;
-		index += 1;
-		var foreground: TileMapLayer = self.get_node("TileMapForeground");
-				
-		foreground.set_cell(path[clamp(index, 0, path.size())], 0, Vector2i(39, 45));
+	
 	pass
--- Scripts/models/map_entity.gd
+++ Scripts/models/map_entity.gd
@@ -5,3 +5,11 @@ var position;
 func _init(position: Vector2i):
 	self.position = position
 	pass
+	
+enum Type {
+	Border,
+	Water,
+	Tree,
+	Rock,
+	House
+}
