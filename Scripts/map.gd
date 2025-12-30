b2a69b8a809c94f00f1375317c8e0e2a7732042b
Piotr Bartoszewicz <piotrek15.pb@gmail.com>
2025-12-30 13:43:35 +0100

    walk

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Map extends Node2D
 
 var entities = [];
 
@@ -7,7 +7,11 @@ var height = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var foreground: TileMapLayer = self.get_node("TileMapForeground");
+	var bg: TileMapLayer = self.get_node("TileMapBackground");
+	var last = bg.get_used_cells().back();
+	width = last.x + 1;
+	height = last.y + 1;
+	
 	get_free_tiles()
 
 	pass # Replace with function body.
@@ -46,7 +50,11 @@ func get_best_path(source: Vector2i, destination: Vector2i) -> Array[Vector2i]:
 			current + Vector2i(0, 1),
 			current + Vector2i(0, -1),
 			current + Vector2i(1, 0),
-			current + Vector2i(-1, 0)
+			current + Vector2i(-1, 0),
+			#current + Vector2i(1, 1),
+			#current + Vector2i(1, -1),
+			#current + Vector2i(-1, -1),
+			#current + Vector2i(-1, 1)
 		].filter(func(v): return (v.x >=0 && v.y >= 0 && v.x < width && v.y < height));
 		#print("n: ", neighbors)
 		for neighbor in neighbors:
@@ -72,10 +80,49 @@ func get_free_tiles() -> Array[Vector2i]:
 	print(background.get_used_cells().size(), " ", border_tiles.size())
 	print(positions.size())
 
-	return Array();
+	var ret: Array[Vector2i] = [];
+	return ret;
 
 func add_entity(entity: MapEntity):
 	pass
+
+const TAP_THRESHOLD = 0.2;
+var touch_start_time = 0;
+var has_moved = false;
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenDrag: 
+		has_moved = true;
+		print(event.relative);
+		var camera = (self.get_parent().get_node("Camera2D") as Camera2D);
+		camera.position -= event.relative;
+	
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			has_moved = false;
+			touch_start_time = Time.get_ticks_msec() / 1000.0;
+		else: 
+			var player: Cat = self.get_parent().get_node("Player");
+			var camera = (self.get_parent().get_node("Camera2D") as Camera2D);
+			var time = Time.get_ticks_msec() / 1000.0 - touch_start_time;
+			print(time);
+			if time < TAP_THRESHOLD && !has_moved:
+				player.move(self.translare_px_to_coords(event.position + camera.position - Vector2(192, 108)));
+	
+	pass
+
+func translate_coord_to_px(pos: Vector2i) -> Vector2i:
+	return pos * 16 + Vector2i(8, 8);
+
+func translate_coords_to_px(pos: Array[Vector2i]) -> Array[Vector2i]:
+	var ret: Array[Vector2i] = [];
+	for p in pos:
+		ret.append(self.translate_coord_to_px(p));
+	return ret
+
+func translare_px_to_coords(pos: Vector2i) -> Vector2i:
+	return Vector2i(pos / 16)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ Scripts/player.gd
@@ -0,0 +1,41 @@
+class_name Cat extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.play("idle")
+	pass # Replace with function body.
+
+var path: Array[Vector2i];
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass 
+	
+func _physics_process(delta: float) -> void:
+	if !path.is_empty():
+		var prev = self.position;
+		#var d = -(self.position - Vector2(path.get(0)));
+		var d = self.position.direction_to(path.get(0))
+		#print("d:", d);
+		self.position += d * delta * (60./8.);
+		#print("dirs: ", self.position.direction_to(path.get(0)), prev.direction_to(path.get(0)))
+		if self.position.direction_to(path.get(0)) != prev.direction_to(path.get(0)) || d.length() < 0.1:
+			#print("overshot")
+			self.position = path.get(0);
+		if self.position == Vector2(path.get(0)):
+			path.pop_front();
+			if path.is_empty():
+				self.play("idle");
+	pass
+
+func move(position: Vector2i):
+	self.play("run");
+	var map = self.get_parent().get_node("Map") as Map;
+	path = map.get_best_path(map.translare_px_to_coords(self.position), position);
+	print(path)
+	path = map.translate_coords_to_px(path);
+	print(path);
+	#self.position = Vector2(path.back())
+	print(self.position)
+	
+	pass
