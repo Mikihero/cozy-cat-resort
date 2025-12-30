65790cadbc92f44aee7bb4a5d192a1e435cb78f4
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2025-12-30 15:44:52 +0100

    Popups (#1)
    * quick confirm and ok fix
    
    * uadpate uids
    
    ---------
    
    Co-authored-by: lunaru100 <lunaruteyvat@gmail.com>

---
+++ Scripts/confirm.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+var message_text = ""
+var action_to_execute: Callable
+
+
+func  _ready() -> void:			
+	$ColorRect/PanelContainer/VBoxContainer/Message.text = message_text
+
+func _on_cancel_pressed() -> void:
+	queue_free()
+
+func _on_confirm_pressed() -> void:
+	if action_to_execute.is_valid():
+		action_to_execute.call()
+	
+	queue_free()
+
+	
