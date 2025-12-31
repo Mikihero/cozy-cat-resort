3a927d396c1d19ab419132f91562a8bc1c70cce1
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2025-12-31 13:11:43 +0100

    update script

--- Scripts/trade_manager.gd
+++ Scripts/trade_manager.gd
@@ -6,18 +6,23 @@ var items_array = ["wood", "clay", "stone"]
 @export var wood_texture: Texture2D
 @export var stone_texture: Texture2D
 
-@onready var item_textures_dict = {
+var item_textures_dict: Dictionary
+
+# Called when the node enters the scene tree for the first time.
+func _enter_tree() -> void:
+	items_array.shuffle()
+	item_textures_dict = {
 	"wood": wood_texture,
 	"clay": clay_texture,
 	"stone": stone_texture,
-}
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	items_array.shuffle()
+	}
 	
 	print(item_textures_dict)
 	print(clay_texture)
+	print(items_array)
+
+func _ready() -> void:
+	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
