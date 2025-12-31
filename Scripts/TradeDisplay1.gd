812767ca9d39d724badd362d54c0bdeac33b829e
Miki_hero <100715076+Mikihero@users.noreply.github.com>
2025-12-31 01:44:07 +0100

    deleted unused assets and work on wandering traders

--- Scripts/TradeDisplay1.gd
+++ Scripts/TradeDisplay1.gd
@@ -5,11 +5,14 @@ extends Button
 @onready var output_background = $"horizontal container/output item node/output item group node/item background texture"
 @onready var input_group_node = $"horizontal container/input item node/input item group node"
 @onready var output_group_node = $"horizontal container/output item node/output item group node"
+@onready var input_price_tag = $"input price tag background"
 
 @export var arrow_texture_normal: Texture2D
 @export var arrow_texture_pressed: Texture2D
 @export var item_background_normal: Texture2D
 @export var item_background_pressed: Texture2D
+@export var price_tag_normal: Texture2D
+@export var price_tag_pressed: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,6 +29,7 @@ func _on_button_down() -> void:
 	input_group_node.position.y += 6
 	output_background.texture = item_background_pressed
 	output_group_node.position.y += 6
+	input_price_tag.texture = price_tag_pressed
 	
 func _on_button_up() -> void:
 	arrow.texture = arrow_texture_normal
@@ -33,3 +37,4 @@ func _on_button_up() -> void:
 	input_group_node.position.y -= 6
 	output_background.texture = item_background_normal
 	output_group_node.position.y -= 6
+	input_price_tag.texture = price_tag_normal
